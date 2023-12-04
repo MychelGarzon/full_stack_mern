f35c27a17431f12047a02079e3217c1d8b55ab7e
Mychel Garzon <mtjomy7@gmail.com>
2023-12-04 14:52:30 +0200

    deleting id from the card

--- frontend/src/components/home/BookSingleCard.jsx
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -12,7 +12,6 @@ const BookSingleCard = ({ book }) => {
 
     return (
         <div className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'>
-            <h4 className='my-2 text-gray-500'>{book._id}</h4>
             <div className='flex justify-start items-center gap-x-2'>
                 <PiBookOpenTextLight className='text-red-300 text-2xl' />
                 <h2 className='my-1'>{book.title}</h2>
